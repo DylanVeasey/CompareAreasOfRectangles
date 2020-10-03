ba4d13f85b712c0ef507e4dfef37945fe97a631e
Dylan Veasey <43819824+DylanVeasey@users.noreply.github.com>
2020-10-03 11:24:03 +0100

    Finsihed Project

---
+++ CompareAreaOfRectangles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareAreaOfRectangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rectangle1 = new Rectangle();
+            Rectangle Rectangle2 = new Rectangle();
+
+            Rectangle1.GetWidth();
+            Rectangle1.GetLength();
+            Rectangle1.CalculateArea();
+
+            Rectangle2.GetWidth();
+            Rectangle2.GetLength();
+            Rectangle2.CalculateArea();
+
+            if(Rectangle1.area > Rectangle2.area)
+            {
+                Console.WriteLine("Rectangle 1 is bigger");
+            }else if (Rectangle1.area < Rectangle2.area)
+            {
+                Console.WriteLine("Rectangle 2 is bigger");
+            }
+            else
+            {
+                Console.WriteLine("The rectangles are the same size");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CompareAreaOfRectangles/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareAreaOfRectangles
+{
+    class Rectangle
+    {
+        public int width;
+        public int length;
+        public int area;
+
+        public void GetWidth()
+        {
+            Console.WriteLine("What is the width of the rectangle?:");
+            width = Int32.Parse(Console.ReadLine());
+        }
+        public void GetLength()
+        {
+            Console.WriteLine("What is the length of the rectangle?:");
+            length = Int32.Parse(Console.ReadLine());
+        }
+        public void CalculateArea()
+        {
+            area = width * length;
+        }
+    }
+}
